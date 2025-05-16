729ba1bfd9fbfa11cace1be87d430d29bfde3eec
90dy <90dy@proton.me>
2025-05-16 11:44:18 +0200

    test(gen): variables backslash
    Signed-off-by: 90dy <90dy@proton.me>

---
+++ src/gen/fixtures/test-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+NODE_NAME=$(hostname)
+EXTERNAL_IP=$(hostname -I | tr ' ' '\n' | head -1)
+// # 3. Patch node status (run as cluster-admin)
+sudo kubectl --kubeconfig ${kubeconfigPath} patch node ${NODE_NAME} \
+--subresource=status \
+--type=json \
+-p="[{'op':'add','path':'/status/addresses/-','value':{'type':'ExternalIP','address':'${EXTERNAL_IP}'}}]"
+# 4. Remove cloud provider taint
+sudo kubectl --kubeconfig ${kubeconfigPath} taint node ${NODE_NAME} node.cloudprovider.kubernetes.io/uninitialized-
